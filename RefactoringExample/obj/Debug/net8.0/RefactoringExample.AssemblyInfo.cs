1c7ca8cbe456215761a1042f8bc0e93468c55e47
Gennady Kirilin <kirilingd@10.215.140.95-vpn.dhcp.yndx.net>
2025-11-30 21:44:48 +0300

    refactoring

--- RefactoringExample/obj/Debug/net8.0/RefactoringExample.AssemblyInfo.cs
+++ RefactoringExample/obj/Debug/net8.0/RefactoringExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefactoringExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c82dffa69b0f30ebfb17c189079ad46b1a0a39")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefactoringExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefactoringExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
